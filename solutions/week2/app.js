1ca440dc611ee4b2f00ef8f1da5eff7cd73307e9
Sergiu Goncear <goncear.sergiu@gmail.com>
2017-02-01 18:22:03 +0200

    Solution for week2 assignment

---
+++ solutions/week2/app.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+    angular.module('ShoppingListCheckOff',[])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService']
+    function ToBuyController(ShoppingListCheckOffService) {
+      var buyController = this;
+      buyController.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
+
+      buyController.boughtItem = function(itemIndex) {
+        ShoppingListCheckOffService.boughtItem(itemIndex)
+      }
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService']
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+      var boughtController = this;
+      boughtController.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    function ShoppingListCheckOffService() {
+      var service = this;
+
+      var toBuyItems =
+        [{name: "Cookies", quantity: 10},
+          {name: "Chips", quantity: 4},
+          {name: "Sugary Drinks", quantity: 5}];
+      var boughtItems = [];
+
+      service.getToBuyItems = function() {
+        return toBuyItems;
+      }
+
+      service.getBoughtItems = function() {
+        return boughtItems;
+      }
+
+      service.boughtItem = function(itemIndex) {
+        boughtItems.push(toBuyItems[itemIndex]);
+        toBuyItems.splice(itemIndex, 1);
+      }
+    }
+
+})()
